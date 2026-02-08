e411acaa8f7a3d4c5b9c49dc81a8942a195c62d2
Spyei <caio.salamao1234@gmail.com>
2026-02-08 00:02:02 -0400

    feat: validator

--- src/models/user/create.rs
+++ src/models/user/create.rs
@@ -1,10 +1,15 @@
 use serde::{Serialize, Deserialize};
 use mongodb::bson::oid::ObjectId;
+use validator::Validate;
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Validate, Serialize, Deserialize, Debug, Clone)]
 pub struct CreateUserRequest {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
+    
+    #[validate(length(min = 1, max = 100))]
     pub name: String,
+    
+    #[validate(email)]
     pub email: String,
 }
